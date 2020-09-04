d0cdeaa713a3d38ddd1b2fc5af1ee96130ce448d
Natalia Wit <natwit95@gmail.com>
2020-09-03 23:13:30 -0400

    palindrome solution 2

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,8 +8,14 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-    const reversed = str.split('').reverse().join('')
-    return str === reversed
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i  -1]
+    })
 }
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+// 	const reversed = str.split("").reverse().join("");
+// 	return str === reversed;
+// }
