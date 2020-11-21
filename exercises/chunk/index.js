cec7be493f273e8293c141dcba25e06d2eafed7a
Natalia Wit <natwit95@gmail.com>
2020-11-20 21:55:20 -0500

    second solution for chunk algo

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -9,18 +9,31 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    const chunked = []
-    for (let element of array) {
-        const last = chunked[chunked.length - 1]
-        
-        if (!last || last.length === size) {
-            chunked.push([element])
-        } else {
-            last.push(element)
-        }
-    }
-    return chunked
+	const chunked = [];
+	let index = 0;
+
+	while (index < array.length) {
+		chunked.push(array.slice(index, index + size));
+		index += size;
+	}
+
+	return chunked;
 }
 
 module.exports = chunk;
- 
+
+//first try
+
+// function chunk(array, size) {
+// 	const chunked = [];
+// 	for (let element of array) {
+// 		const last = chunked[chunked.length - 1];
+
+// 		if (!last || last.length === size) {
+// 			chunked.push([element]);
+// 		} else {
+// 			last.push(element);
+// 		}
+// 	}
+// 	return chunked;
+// }
